1d7c01305f3c0dc47196451f9f515930a5de7e52
cxbing1 <983121062@qq.com>
2017-06-03 17:21:52 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+  
+  var r=email.split("@");
+  if(r==suffixes)
+    return true;
+  else
+    return false;
+  
+  
 };
