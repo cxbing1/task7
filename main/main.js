b65f64a044c8fe708f91175f45bb2e8fc83317af
cxbing1 <983121062@qq.com>
2017-06-04 17:19:25 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@ module.exports = function main(email, suffixes) {
   // Write your code here
   
   var r=email.split("@");
-  if(r[r,length-1]==suffixes)
+  if(r[r.length-1]==suffixes)
     return true;
   else
     return false;
